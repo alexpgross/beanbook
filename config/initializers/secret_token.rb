4a69216e4ff2b6a7ea53f02b771edaa59b4ca6bb
Flex Philips <flexphilips@gmail.com>
2013-09-30 22:07:42 -0700

    uploading beanbook after initializing statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beanbook::Application.config.secret_key_base = '4ae978d73ef2c7cb7bee0271fa57454c70d3d64ec31452c90cd17a65ffd24219df94ea094b3a14f1c2d7fa8d50b27fa9c96fda4c59d5c70088b308724ad3f727'
